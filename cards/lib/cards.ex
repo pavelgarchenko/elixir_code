effc858134837cb8ab6c6fe33dc66547543af70c
Pavel <vargan.pg@gmail.com>
2019-11-28 02:53:49 -0500

    load deck from file; improve case

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -49,11 +49,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, content} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(content)
-      :error -> "File does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File '#{filename}' does not exist"
     end
   end
 end
