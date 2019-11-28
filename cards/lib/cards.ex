751f8d66c57a01eee3ff647a44dce88b8dfdfb1d
Pavel <vargan.pg@gmail.com>
2019-11-28 02:52:05 -0500

    init commit; upto save to file

---
+++ cards/lib/cards.ex
@@ -0,0 +1,50 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Create deck.
+
+  ## Examples
+
+      $ iex -S mix
+      iex> Cards.create_deck()
+      ["Ace", "Two", "Three"]
+
+      recompile
+      'arity' - is a term for number of args
+      arity of one - one arg
+      or arity one
+  """
+
+  def create_deck do
+    # in functional programming the value is
+    # return w/o assigning it to an isnstance variable
+    ranks = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+
+    for suit <- suits, rank <- ranks do
+      "#{rank} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    # Patern Matching is Elixir's replacement
+    # for variable assignment
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+end
