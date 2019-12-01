172b5372eeb4b383644ec235cd36aa70037ad3e5
Pavel <vargan.pg@gmail.com>
2019-11-30 22:16:22 -0500

    37.Added hash_input function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,23 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Identicon generator.
+  Create an image of 5x5 grid of 50x50px squares with uniq pattern
   """
+
+  def main(input) do
+    input
+    |> Identicon.hash_input()
+  end
 
   @doc """
-  Hello world.
-
+  Generate MD5 hash from string
   ## Examples
 
-      iex> Identicon.hello()
-      :world
-
+      iex> Identicon.hash_input("Pavel")
+      [152, 48, 128, 95, 215, 148, 211, 184, 203, 238, 34, 254, 137, 143, 239, 183]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
