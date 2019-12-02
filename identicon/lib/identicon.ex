108f7dd271f555848e744a1e2592641475e4ec92
Pavel <vargan.pg@gmail.com>
2019-12-01 22:31:28 -0500

    49.Draw image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -11,6 +11,18 @@ defmodule Identicon do
     |> Identicon.build_grid
     |> Identicon.filter_odd_squares
     |> Identicon.build_pixel_map
+    |> Identicon.draw_image
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250,250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
