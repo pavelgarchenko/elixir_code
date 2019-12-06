1cdbfc02ec919e9da9ce0fd0cc1960a2d30bd47b
Pavel <vargan.pg@gmail.com>
2019-12-05 22:44:21 -0500

    77.Add form to create new topic; add controller and model

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,7 +1,14 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.TopicController
+
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
   end
 end
