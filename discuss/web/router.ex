c4cebe411e78c659671b5d1d7a4f340f39d2c5cc
Pavel <vargan.pg@gmail.com>
2019-12-07 23:47:48 -0500

    90.Added show, edit, update logic

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -19,6 +19,9 @@ defmodule Discuss.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id", TopicController, :show
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
