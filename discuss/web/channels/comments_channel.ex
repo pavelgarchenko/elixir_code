96ab0c32c415439c9fb328b4cb0bf7c14098a5b9
Pavel <vargan.pg@gmail.com>
2019-12-24 18:42:57 -0500

    preload topic comments assoc

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -2,12 +2,13 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
   alias Discuss.{Topic, Comment}
 
-
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(:comments)
 
-    {:ok, %{}, assign(socket, :topic, topic)}
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
   def handle_in(name, %{"content" => content}, socket) do
