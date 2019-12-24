776ba28bb295d96552f5337b3f9eafe89b568307
Pavel <vargan.pg@gmail.com>
2019-12-24 18:30:52 -0500

    add functionality to post comments

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -57,8 +57,14 @@ const createSocket = (topicId) => {
   // Now that you are connected, you can join channels with a topic:
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { content: content });
+  });
 }
 
 window.createSocket = createSocket;
