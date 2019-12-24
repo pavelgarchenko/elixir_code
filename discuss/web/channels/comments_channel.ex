88bf3b9420221d4c074eaae12439ec840ad82acc
Pavel <vargan.pg@gmail.com>
2019-12-24 16:12:32 -0500

    Added createSocket  function

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -2,6 +2,8 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
+    IO.puts("++++++++")
+    IO.puts(name)
     {:ok, %{hey: "there"}, socket}
   end
 
